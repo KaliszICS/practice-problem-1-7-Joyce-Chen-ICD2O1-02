ff171eabdd1f71efb2e5b6ff26c3a95e3a46dd64
Joyce-Chen-ICD2O1-02 <joyce.chen@student.tdsb.on.ca>
2024-09-26 16:55:14 +0000

    a

--- PP1_7.py
+++ PP1_7.py
@@ -1,25 +1,40 @@
-
+'''
+    Lesson: Booleans
+    Author: Joyce
+    Date Created: Sept 26, 2024
+    Date Last Modified: Sept 26, 2024
+'''
 
 def q1():
-  #Write code here
+  bool1 = True
+  print(bool1)
 
 def q2():
-  #Write code here
+  bool2 = int(input("Input an integer: "))
+  bool2 = 5 < bool2
+  print(bool2)
 
 def q3():
-  #Write code here
+  bool3 = input("Input the letter a: ")
+  bool3 = bool3 == "a" 
+  print(bool3)
 
 def q4():
-  #Write code here
+  bool4 = input("Input a word earlier in the dictionary than google: ")
+  bool4 = bool4 < "google"
+  print(bool4)
 
 def q5():
-  #Write code here
+  int1 = int(input("Input an integer: "))
+  int2 = int(input("Input another integer: "))
+  bool5 = int1*int2 > 40
+  print(f"Your numbers multiplied together are greater than 40: {bool5}")
 
 #Do edit the code below
 #Comment the lines below when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
